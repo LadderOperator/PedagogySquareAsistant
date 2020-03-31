bb4461fdfe7c81540e1f322f4859c0bada3420b1
LadderOperator <tz_ji@qq.com>
2020-03-31 17:49:49 +0800

    为学生增加显示下载功能。

--- catchHomework.js
+++ catchHomework.js
@@ -1,55 +1,78 @@
-var homeworkPara = homework.question_list
 
-//console.log(homeworkPara)
+try {
+  
+  var homeworkPara = homework.question_list
 
-var questionCount = homeworkPara.length
+  console.log("捕捉到作业文件")
 
-//console.log(questionCount)
+  //console.log(homeworkPara)
 
-var fileLinks = []
-var imgLinks = []
-        
-var attachElements = document.querySelectorAll('div.records-exam-section.s-work-section > div > div > div:not(.change) > a')
+  var questionCount = homeworkPara.length
 
-//console.log(attachElements)
+  //console.log(questionCount)
 
-for (let i = 0; i < questionCount; i++) {
-        
-    var attachment = homeworkPara[i].answer_record.attach_info
+  var fileLinks = []
+  var imgLinks = []
+          
+  var attachElements = document.querySelectorAll('div.records-exam-section.s-work-section > div > div > div:not(.change) > a')
 
-    var attachCount = attachment.length
-    for (let j = 0; j < attachCount; j++) {
-      var attachInfo = []
+  //console.log(attachElements)
 
-      var fName = attachment[j].filename
-      var tmpURL = attachment[j].path
-      var fExt = attachment[j].ext
+  for (let i = 0; i < questionCount; i++) {
+          
+      var attachment = homeworkPara[i].answer_record.attach_info
 
-      attachInfo.push(fName)
-      attachInfo.push(fExt)
-      attachInfo.push(tmpURL)
+      var attachCount = attachment.length
+      for (let j = 0; j < attachCount; j++) {
+        var attachInfo = []
 
-      for (let k = 0; k < attachElements.length; k++) {
-        if (attachElements[k].textContent.indexOf(fName) > -1){
-          attachInfo.push(attachElements[k])
+        var fName = attachment[j].filename
+        var tmpURL = attachment[j].path
+        var fExt = attachment[j].ext
+
+        attachInfo.push(fName)
+        attachInfo.push(fExt)
+        attachInfo.push(tmpURL)
+
+        for (let k = 0; k < attachElements.length; k++) {
+          if (attachElements[k].textContent.indexOf(fName) > -1){
+            attachInfo.push(attachElements[k])
+          }
+          
         }
-        
-      }
-      if ('jpg|jpeg|png|gif'.indexOf(fExt.toLowerCase()) > -1 ) {
-        imgLinks.push(attachInfo)
-      }else {
-        fileLinks.push(attachInfo)
-      }
+        if ('jpg|jpeg|png|gif'.indexOf(fExt.toLowerCase()) > -1 ) {
+          imgLinks.push(attachInfo)
+        }else {
+          fileLinks.push(attachInfo)
+        }
 
+      }
     }
+
+  imgLinks.forEach(function (link) {
+    var img = document.createElement('img')
+    img.src = link[2]
+    link[3].parentNode.insertBefore(img, link[3].nextSibling)
+  })
+  fileLinks.forEach(function (link) {
+    link[3].href = link[2]
+    link[3].target = '_blank'
+  })
+}
+catch(hmerr){
+  console.error(hmerr)
+}
+
+
+try {
+  var resource = lessonindex.lesson_list
+  console.log("捕捉到课件文件")
+
+  for (let ri = 0; ri < resource.length; ri++) {
+    resource[ri].can_download = 1
   }
 
-imgLinks.forEach(function (link) {
-  var img = document.createElement('img')
-  img.src = link[2]
-  link[3].parentNode.insertBefore(img, link[3].nextSibling)
-})
-fileLinks.forEach(function (link) {
-  link[3].href = link[2]
-  link[3].target = '_blank'
-})
+}
+catch(lserr){
+  console.error(lserr)
+}
