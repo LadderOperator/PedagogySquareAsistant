4d7a6218dd2e6813a38d44d49cc37384d57c663e
LadderOperator <tz_ji@qq.com>
2020-04-06 22:24:01 +0800

    更新了旋转图片、移动评阅位置的功能

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file: "contentscript.js"});
  });
+ 
--- catchHomework.js
+++ catchHomework.js
@@ -1,6 +1,13 @@
 
-try {
-  
+
+var currentURL = window.location.href;
+var lessonStr = "S-Lesson-index"
+var TWorkStr = "T-Work-treviewinfo"
+
+if (currentURL.includes(TWorkStr)) {
+
+  document.getElementsByClassName('caozuo')[0].setAttribute("style", "position:unset;text-align:right;");
+
   var homeworkPara = homework.question_list
 
   console.log("捕捉到作业文件")
@@ -50,29 +57,88 @@ try {
     }
 
   imgLinks.forEach(function (link) {
+    var br1 = document.createElement('br')
+    var br2 = document.createElement('br')
+    var canvas = document.createElement('canvas')
     var img = document.createElement('img')
     img.src = link[2]
-    link[3].parentNode.insertBefore(img, link[3].nextSibling)
+    img.className = "previewImg"
+    canvas.className = "previewCanvas-0"
+    var ctx = canvas.getContext('2d')
+    img.onload = function(){
+      
+      canvas.width = link[3].parentElement.offsetWidth
+      canvas.height = link[3].parentElement.offsetWidth*img.height/img.width
+      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height) 
+
+    }
+    canvas.addEventListener('click',function () {
+      var currentDeg = parseInt((/[0-9]+/i).exec(this.className))
+      var newDeg = (currentDeg + 90) % 360
+      var ctx = this.getContext('2d')
+      var img = this.children[0]
+      switch (newDeg) {
+        case 90:
+          ctx.clearRect(0,0,canvas.width,canvas.height)
+          canvas.height = this.parentElement.offsetWidth*img.width/img.height
+          ctx.rotate(1/2*Math.PI)
+          ctx.translate(0,-canvas.width)
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.height, canvas.width) 
+          ctx.translate(0,canvas.width)
+          ctx.rotate(-1/2*Math.PI)
+          break;
+
+        case 180:
+          ctx.clearRect(0,0,canvas.width,canvas.height)
+          canvas.height = this.parentElement.offsetWidth*img.height/img.width
+          ctx.rotate(Math.PI)
+          ctx.translate(-canvas.width,-canvas.height)
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height) 
+          ctx.translate(canvas.width,canvas.height)
+          ctx.rotate(-Math.PI)
+          break;
+
+        case 270:
+          ctx.clearRect(0,0,canvas.width,canvas.height)
+          canvas.height = this.parentElement.offsetWidth*img.width/img.height
+          ctx.rotate(-1/2*Math.PI)
+          ctx.translate(-canvas.height,0)
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.height, canvas.width) 
+          ctx.translate(canvas.height,0)
+          ctx.rotate(1/2*Math.PI)
+          break;
+
+        case 0:
+          canvas.height = this.parentElement.offsetWidth*img.height/img.width
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height) 
+          break;
+        
+        default:
+          break;
+      }
+
+      this.className = "previewCanvas-" + newDeg.toString()
+    })
+    canvas.appendChild(img)
+    link[3].parentNode.insertBefore(br1, link[3].nextSibling)
+    link[3].parentNode.insertBefore(canvas, link[3].nextSibling)
+    link[3].parentNode.insertBefore(br2, link[3].nextSibling)
   })
+
   fileLinks.forEach(function (link) {
     link[3].href = link[2]
     link[3].target = '_blank'
   })
-}
-catch(hmerr){
-  console.error(hmerr)
+
 }
 
 
-try {
+if (currentURL.includes(lessonStr)) {
   var resource = lessonindex.lesson_list
   console.log("捕捉到课件文件")
 
   for (let ri = 0; ri < resource.length; ri++) {
     resource[ri].can_download = 1
   }
+}
 
-}
-catch(lserr){
-  console.error(lserr)
-}
